2bf2891a82187fc17c4a12fcc4078ed87b80a47b
pasindumanahara <manahara2003@gmail.com>
2025-09-20 22:11:53 +0530

    database update

--- server/test.php
+++ server/test.php
@@ -1,7 +1,7 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "1234", "login_test");
+    $conn = mysqli_connect("localhost", "root", "1234", "eduflow");
     $password = password_hash("1234",PASSWORD_DEFAULT);
-    $sql = "insert into users (username,password) values('manahara','$password');";
+    $sql = "insert into login (username,password) values('a0000','$password');";
     try {
         mysqli_query($conn,$sql);
     } catch (mysqli_sql_exception) {
